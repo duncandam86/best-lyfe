d35040d5471520c97b91c45450f65f10f708f7b6
Bill W <42258241+wwunsch23@users.noreply.github.com>
2019-02-09 15:01:41 -0600

    Updated pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Route, Link } from "react-router-dom";
 // components
-import Signup from "./components/sign-up";
-import Login from "./components/login-form";
+import Signup from "./pages/SignUp";
+import Login from "./pages/Login";
 import Navbar from "./components/navbar";
 import Home from "./pages/Home";
 // styles

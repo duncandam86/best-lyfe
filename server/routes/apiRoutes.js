edc2696c7e1fe6e5cfee3c1be5cf6625071e391f
jmo <40837639+jmo927@users.noreply.github.com>
2019-02-10 16:39:23 -0600

    add Habit

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,17 +1,19 @@
 const db = require("../models");
-const sequelize = require("sequelize");
-const Op = Sequelize.Op;
+// const sequelize = require("sequelize");
+// const Op = Sequelize.Op;
 const moment = require("moment");
 
 module.exports = function(app) {
   app.post("/api/newHabit", function(req, res) {
-    console.log("Habit Data:");
-    console.log(req.body);
-    Habit.create({
+    // console.log("Habit Data:");
+    // console.log(req.body);
+    db.Habits.create({
       title: req.body.title,
       time: req.body.time,
       // frequency: req.body.frequency,
-      comment: req.body.comments
+      comment: req.body.comment,
+      UserId: 1,
+      recordArray: 0
     }).then(function(results) {
       res.json(results);
     });
@@ -21,7 +23,7 @@ module.exports = function(app) {
 
   app.get("/api/habits/:id", function(req, res) {});
 
-  app.update("/api/habits/:id", function(req, res) {});
+  // app.update("/api/habits/:id", function(req, res) {});
 
   app.delete("/api/habits/:id", function(req, res) {
     console.log("Habit ID:");

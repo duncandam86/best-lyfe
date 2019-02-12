8aff0e7deafa1233653549ed383152dbedab6956
Tyler G <guthriet1230@gmail.com>
2019-02-11 19:28:05 -0600

    navbar and habits page style fixes

--- client/src/pages/Habits/index.js
+++ client/src/pages/Habits/index.js
@@ -3,7 +3,8 @@ import { Redirect } from "react-router-dom";
 
 import Navbar from "../../components/Navbar";
 import BodyWrapper from "../../components/Bodywrapper";
-import HabitsComponent from "../../components/HabitsComponent";
+// import HabitsComponent from "../../components/HabitsComponent";
+import DropDownComponent from "../../components/DropDownComponent";
 //import "../../styles/shared.scss";
 import "./style.scss";
 import axios from "axios";
@@ -30,7 +31,39 @@ class Habits extends Component {
         <Navbar />
         <div className="container">
           <BodyWrapper title1="Your" title2="Habits">
-            <HabitsComponent />
+            <div id="habits">
+              <DropDownComponent />
+              <img src="../../images/AddButton.png" />
+            </div>
+            <div id="habits-body">
+              <div id="habits-header">
+                <h2>Workout {/* {props.Habit} */}</h2>
+                <div className="remove">
+                  <h3>
+                    <a
+                    // href={"/habits/" + habit._id}
+                    >
+                      remove
+                    </a>
+                  </h3>
+                </div>
+              </div>
+              <hr />
+
+              {/* <h3>Progress:</h3><figure></figure> */}
+
+              <h4>Name:</h4>
+              <p>Workout{/* {props.Name} */}</p>
+              <h4>Time:</h4>
+              <p>6:30am{/* {props.Time} */}</p>
+
+              <h4>Comments:</h4>
+              <p>
+                Mix of Legs, Arms, Cardio, Chest/Back{/* {props.Comments} */}
+              </p>
+
+              <button id="complete">Complete</button>
+            </div>
           </BodyWrapper>
         </div>
       </>

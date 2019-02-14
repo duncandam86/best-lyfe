60f1f3e51dde55a95895a2d2e11c8bcfab332989
Tyler G <guthriet1230@gmail.com>
2019-02-13 19:03:37 -0600

    Navbar update - with Clickout HOC

---
+++ client/src/components/HOC/withClickOutside.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+export default function withClickOutside(WrappedComponent) {
+  return class ClickOutsideHoc extends Component {
+    state = {
+      isOpen: false
+    };
+
+    toggle = event => this.setState({ isOpen: !this.state.isOpen });
+
+    constructor(props) {
+      super(props);
+
+      this.setWrapperRef = this.setWrapperRef.bind(this);
+      this.handleClickOutside = this.handleClickOutside.bind(this);
+    }
+
+    componentDidMount() {
+      document.addEventListener("mousedown", this.handleClickOutside);
+    }
+
+    componentWillUnmount() {
+      document.removeEventListener("mousedown", this.handleClickOutside);
+    }
+
+    /**
+     * Set the wrapper ref
+     */
+    setWrapperRef(node) {
+      this.wrapperRef = node;
+    }
+
+    /**
+     * Alert if clicked on outside of element
+     */
+    handleClickOutside(event) {
+      console.log(this.wrapperRef);
+      if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+        this.setState({ isOpen: false });
+      }
+    }
+
+    render() {
+      return (
+        <div ref={this.setWrapperRef}>
+          <WrappedComponent
+            isOpen={this.state.isOpen}
+            toggleOpen={this.toggle}
+          />
+        </div>
+      );
+    }
+  };
+}
